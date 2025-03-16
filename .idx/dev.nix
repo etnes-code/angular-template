5ff347ac59b23145fbb57940f5093160c8e7d2bc
sente.quentin <sente.quentin@gmail.com>
2025-03-16 18:44:41 +0000

    provide http client

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,9 +6,12 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
+    pkgs.chromium
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    CHROME_BIN="/usr/bin/chromium";
+  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
