ccdc638c211268b90925713e06b1b20cb70652e8
sente.quentin <sente.quentin@gmail.com>
2025-03-17 19:48:39 +0000

    fix test

--- src/app/apis/api.service.spec.ts
+++ src/app/apis/api.service.spec.ts
@@ -1,16 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 
 import { ApiService } from './api.service';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
 
 describe('ApiService', () => {
   let service: ApiService;
+  let http: jasmine.SpyObj<HttpClient>;
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ApiService, provideHttpClient()]
+    });
+
     service = TestBed.inject(ApiService);
+    http = jasmine.createSpyObj('HttpClient', ['get'])
+  });
+
+  afterEach(() => {
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
+  });  
 });
+
+
